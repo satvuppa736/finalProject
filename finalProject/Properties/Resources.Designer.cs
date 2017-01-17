38f78fdbeec90841574aa56c2acf9927b6352e61
Laptop <satvuppa736@ed.amdsb.ca>
2017-01-17 17:35:39 -0500

    Commit

--- finalProject/Properties/Resources.Designer.cs
+++ finalProject/Properties/Resources.Designer.cs
@@ -71,15 +71,6 @@ namespace finalProject.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
-        /// </summary>
-        internal static System.IO.UnmanagedMemoryStream Game_Theme {
-            get {
-                return ResourceManager.GetStream("Game_Theme", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap npcCharacter {
@@ -126,6 +117,15 @@ namespace finalProject.Properties {
             get {
                 object obj = ResourceManager.GetObject("screen2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream themeSong {
+            get {
+                return ResourceManager.GetStream("themeSong", resourceCulture);
             }
         }
     }
